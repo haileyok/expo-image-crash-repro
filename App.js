9500ed0e8d56cd96475ccf4fce0292a49b5bbbc9
Hailey <me@haileyok.com>
2024-04-25 02:10:05 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Image} from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source="https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:b3psimnw5xxta2n4h5qzxu55/bafkreietuymak7njxkkr35ecjwlkhk3risz3zmfmvwak5hlji7agvdhcwm@jpeg"
+        enableLiveTextInteraction
+        style={{aspectRatio: 1, height: 600, width: '100%'}}
+      />
     </View>
   );
 }
